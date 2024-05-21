6623fe620aa957d62fdf256d5e8342ef86bc7dc4
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-21 23:46:56 +0530

    ModelViewsets

--- rest_api_watchmate/watchlist_app/api/views.py
+++ rest_api_watchmate/watchlist_app/api/views.py
@@ -117,8 +117,13 @@ class WatchDetailsAPIView(APIView):
         except WatchList.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
+
+class StreamPlatformViewSet(viewsets.ModelViewSet):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer        
+        
  
-class StreamPlatformViewSet(viewsets.ViewSet):
+# class StreamPlatformViewSet(viewsets.ViewSet):
     
     def list(self, request):
         queryset = StreamPlatform.objects.all()
@@ -130,6 +135,35 @@ class StreamPlatformViewSet(viewsets.ViewSet):
         watchlist = get_object_or_404(queryset, pk=pk)
         serializer = StreamPlatformSerializer(watchlist)
         return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = StreamPlatformSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def destroy(self, request, pk=None):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+            platform.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except StreamPlatform.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def update(self, request, pk=None):
+        try:
+            platform = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = StreamPlatformSerializer(platform, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
    
         
 class StreamPlatformListAPIView(APIView):
