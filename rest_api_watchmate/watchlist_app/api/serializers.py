6e002e6d7d26ee1be5409b04371a801008a097ce
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-21 11:14:55 +0530

    Custom Serializer Fields

--- rest_api_watchmate/watchlist_app/api/serializers.py
+++ rest_api_watchmate/watchlist_app/api/serializers.py
@@ -3,6 +3,8 @@ from watchlist_app.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    #Custom Serializer Fields
+    len_name = serializers.SerializerMethodField()
     
     class Meta:
         model = Movie
@@ -10,17 +12,20 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # exclude = ['active']
         
-        def validate(self, data):
-            if data['name'] == data['description']:
-                raise serializers.ValidationError("Title and Description should be different")
-            else:
-                return data
-    
-        def validate_name(self, value):
-            if len(value) < 2:
-                raise serializers.ValidationError("Name is too short")
-            else:
-                return value
+    def get_len_name(self, object):       
+        return len(object.name)  
+        
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different")
+        else:
+            return data
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
         
     
 
