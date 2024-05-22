7bd74afc20aa840146af5f426f2303d046547732
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-22 10:57:46 +0530

    Implemented user model

--- rest_api_watchmate/watchlist_app/api/serializers.py
+++ rest_api_watchmate/watchlist_app/api/serializers.py
@@ -7,6 +7,7 @@ from watchlist_app.models import (
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
     
     class Meta:
         model = Review
--- rest_api_watchmate/watchlist_app/api/views.py
+++ rest_api_watchmate/watchlist_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
@@ -24,10 +25,21 @@ from watchlist_app.api.serializers import (
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
     
+    def get_queryset(self):
+        return Review.objects.all()
+    
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
-        serializer.save(watchlist=watchlist)
+        
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+        
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie!")
+        
+        
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
