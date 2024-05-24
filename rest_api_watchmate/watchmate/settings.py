46f516cf1518acba3bb0edc29e2db94a96afd01b
pradeeprajkk <pradeep.raj@accionlabs.com>
2024-05-24 17:37:17 +0530

    Disable Browsable API access

--- rest_api_watchmate/watchmate/settings.py
+++ rest_api_watchmate/watchmate/settings.py
@@ -154,6 +154,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'PAGE_SIZE': 5,
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 SIMPLE_JWT = {
